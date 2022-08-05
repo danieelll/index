6c392fe0cfac09e9c9f54c3bfbb9828def619a72
daniel20asdasd <danielmaala16@gmail.com>
2022-08-05 11:26:44 +0800

    Country state city

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+//https://www.npmjs.com/package/country-state-city
+//import npm i country-state-city
+
+ 
+const Country = require('country-state-city').Country
+
+console.log(Country.getAllCountries())
+//const State = require('country-state-city').State
+
+console.log(Country.getCountryByCode("US"))
+//console.log(State.getAllStates())
